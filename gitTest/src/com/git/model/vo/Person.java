726d7f2df3e725f2aa5efa0c1b7e7893d06c536d
jeonghyun <jhyeon310@naver.com>
2020-09-15 11:08:46 +0900

    01_ person add properties name,age -je-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -2,5 +2,21 @@ package com.git.model.vo;
 
 public class Person {
 	//회원정보를 저장하는 클래스
+	private String name;
+	private int age;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	
+	
+	
+	
 }
