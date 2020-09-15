559e033be5b593629d5f3574786563ca1e0c8372
jeonghyun <jhyeon310@naver.com>
2020-09-15 11:16:32 +0900

    ㅇ

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -9,11 +9,23 @@ public class Person {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Person(String name, int age) {
-		super();
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
 		this.age = age;
 	}
+
+	
 	
 	
 	
